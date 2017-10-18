d75232dc977be42da13a6db080311f6bd1f6fec5
Itiu <ValeriyBushenev@gmail.com>
2017-10-18 21:57:20 +0300

    * inital

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VEDA_VERSION=5.1.13
+PROJECT_NAME=module-net-complex-route
+
+wget https://github.com/semantic-machines/veda/archive/v$VEDA_VERSION.zip
+unzip v$VEDA_VERSION.zip
+cp -r veda-$VEDA_VERSION/* $PWD 
+mkdir $PWD/ontology/$PROJECT_NAME
+cp -r $PWD/qa/data/* $PWD/ontology/$PROJECT_NAME
+cp -r $PWD/onto/* $PWD/ontology/$PROJECT_NAME
+./control-install.sh
